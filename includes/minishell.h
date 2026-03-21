11568d6a8dae696d8d29304973943f4794608b76
かわ <qwedcxzas13579@gmail.com>
2026-03-21 23:13:06 +0900

    feature: データ構造を定義

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,26 +6,71 @@
 /*   By: takenakatakeshiichirouta <takenakatakes    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 16:57:15 by takenakatak       #+#    #+#             */
-/*   Updated: 2026/03/21 16:57:15 by takenakatak      ###   ########.fr       */
+/*   Updated: 2026/03/21 23:12:37 by takenakatak      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
+# include "libft.h"
 # include <errno.h>
 # include <fcntl.h>
-# include <sys/wait.h>
-# include <sys/stat.h>
-# include <signal.h>
-# include <readline/readline.h>
 # include <readline/history.h>
-# include "libft.h"
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
-extern int	g_signal;
+extern int			g_signal;
+
+typedef enum e_token_type
+{
+	TK_WORD,
+	TK_PIPE,
+	TK_REDIR_IN,
+	TK_REDIR_OUT,
+	TK_HEREDOC,
+	TK_APPEND,
+	TK_EOF,
+}					t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}					t_token;
+
+typedef struct s_redir
+{
+	t_token_type	type;
+	char			*file;
+	struct s_redir	*next;
+}					t_redir;
+
+typedef struct s_cmd
+{
+	char			**argv;
+	t_redir			*redirs;
+	struct s_cmd	*next;
+}					t_cmd;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+typedef struct s_shell
+{
+	t_env			*env;
+	int				last_status;
+}					t_shell;
 
 #endif
