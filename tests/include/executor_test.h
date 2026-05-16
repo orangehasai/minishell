c6937758da39c8cf2bca100ebfac7ee8daa28146
かわ <qwedcxzas13579@gmail.com>
2026-05-17 02:22:32 +0900

    test: 単体builtinコマンドのリダイレクトのテストを実装

--- tests/include/executor_test.h
+++ tests/include/executor_test.h
@@ -61,6 +61,9 @@ int		report_capture_case(char *name, t_capture_expect *expect,
 			t_capture_exec *capture);
 int		report_cd_case(char *name, int actual_status, t_cd_expect *expect,
 			t_shell *shell);
+int		report_cd_file_case(char *name, char *cmdline, int actual_status,
+			t_cd_expect *expect, char *path, char *expected, char *actual,
+			t_shell *shell);
 int		report_file_case(char *name, char *cmdline, int expected_status,
 			int actual_status, char *path, char *expected, char *actual);
 int		report_dual_file_case(char *name, char *cmdline, int expected_status,
@@ -75,6 +78,10 @@ int		test_exec_redir_stdout_append(void);
 int		test_exec_redir_stdin(void);
 int		test_exec_redir_left_to_right(void);
 int		test_exec_redir_heredoc_unsupported(void);
+int		test_exec_builtin_redir_echo(void);
+int		test_exec_builtin_redir_pwd(void);
+int		test_exec_builtin_redir_env(void);
+int		test_exec_builtin_redir_cd(void);
 int		test_exec_builtin_echo(void);
 int		test_exec_builtin_echo_no_newline(void);
 int		test_exec_builtin_echo_multi_n(void);
